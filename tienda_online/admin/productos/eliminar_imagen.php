6cc0e1629e30061a5e5c32fa9dc9795f8c9bec0d
Cmpo152 <zempoaltecajorge152@gmail.com>
2024-06-03 19:15:33 -0600

    Se subio el proyecto

---
+++ tienda_online/admin/productos/eliminar_imagen.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Elimina imagen del producto
+ * Autor: WifiTeam
+ */
+
+require '../config/config.php';
+
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin') {
+    header('Location: ../index.php');
+    exit;
+}
+
+$urlImagen = $_POST['urlImagen'] ?? '';
+
+if ($urlImagen !== '' && file_exists($urlImagen)) {
+    unlink($urlImagen);
+}
